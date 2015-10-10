f0e9be702bea8ffd002303a3dc9dec2aba831de1
Brooklyn Zelenka <be.zelenka@gmail.com>
2015-10-10 00:31:58 -0700

    Remove duplicate definition

--- lib/witchcraft.ex
+++ lib/witchcraft.ex
@@ -1,6 +1,2 @@
 defmodule Witchcraft do
-  # @spec liftA
-  def liftA(function, wrapped_element) do
-    Witchcraft.Applicative.apply(wrapped_element, Witchcraft.Applicative.pure(function))
-  end
 end
