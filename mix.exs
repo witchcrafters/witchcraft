b0d326124436d65991f08d4bee785941ff9d1630
Brooklyn Zelenka <be.zelenka@gmail.com>
2016-12-27 18:57:12 +0900

    Bump type_class

--- mix.exs
+++ mix.exs
@@ -32,8 +32,7 @@ defmodule Witchcraft.Mixfile do
 
         {:operator,   "~> 0.2"},
         {:quark,      "~> 2.1"},
-        {:type_class, github: "expede/type_class", branch: "trying"}
-        # {:type_class, "~> 1.0.0-alpha"}
+        {:type_class, "~> 1.0.0-alpha3"}
       ],
 
       docs: [
