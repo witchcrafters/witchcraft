10570386b09b5e7d7ddc3e2d35a3a21a89b345de
Brooklyn Zelenka <be.zelenka@gmail.com>
2017-08-01 19:33:01 -0700

    More of same

--- lib/witchcraft/monad.ex
+++ lib/witchcraft/monad.ex
@@ -1,145 +1,145 @@
-# import TypeClass
+import TypeClass
 
-# defclass Witchcraft.Monad do
-#   @moduledoc """
-#   Very similar to `Chain`, `Monad` provides a way to link actions, and a way
-#   to bring plain values into the correct context (`Applicative`).
+defclass Witchcraft.Monad do
+  @moduledoc """
+  Very similar to `Chain`, `Monad` provides a way to link actions, and a way
+  to bring plain values into the correct context (`Applicative`).
 
-#   This allows us to view actions in a full framework along the lines of
-#   functor and applicative:
+  This allows us to view actions in a full framework along the lines of
+  functor and applicative:
 
-#                  data ---------------- function ----------------------------> result
-#                    |                      |                                     |
-#        of(Container, data)          of/2, or similar                of(Container, result)
-#                    ↓                      ↓                                     ↓
-#       %Container<data> --- (data -> %Container<updated_data>) ---> %Container<updated_data>
+                 data ---------------- function ----------------------------> result
+                   |                      |                                     |
+       of(Container, data)          of/2, or similar                of(Container, result)
+                   ↓                      ↓                                     ↓
+      %Container<data> --- (data -> %Container<updated_data>) ---> %Container<updated_data>
 
-#   As you can see, the linking function may just be `of` now that we have that.
+  As you can see, the linking function may just be `of` now that we have that.
 
-#   For a nice, illustrated introduction,
-#   see [Functors, Applicatives, And Monads In Pictures](http://adit.io/posts/2013-04-17-functors,_applicatives,_and_monads_in_pictures.html).
+  For a nice, illustrated introduction,
+  see [Functors, Applicatives, And Monads In Pictures](http://adit.io/posts/2013-04-17-functors,_applicatives,_and_monads_in_pictures.html).
 
-#   Having `of` also lets us enhance do-notation with a convenuenct `return` function (see `monad/2`)
+  Having `of` also lets us enhance do-notation with a convenuenct `return` function (see `monad/2`)
 
-#   ## Type Class
+  ## Type Class
 
-#   An instance of `Witchcraft.Monad` must also implement `Witchcraft.Applicative`
-#   and `Wicthcraft.Chainable`.
+  An instance of `Witchcraft.Monad` must also implement `Witchcraft.Applicative`
+  and `Wicthcraft.Chainable`.
 
-#                        Functor     [map/2]
-#                           ↓
-#                         Apply      [ap/2]
-#                         ↓   ↓
-#       [of/2]  Applicative   Chain  [chain/2]
-#                         ↓   ↓
-#                         Monad
-#                          [_]
-#   """
+                       Functor     [map/2]
+                          ↓
+                        Apply      [convey/2]
+                        ↓   ↓
+      [of/2]  Applicative   Chain  [chain/2]
+                        ↓   ↓
+                        Monad
+                         [_]
+  """
 
-#   extend Witchcraft.Applicative
-#   extend Witchcraft.Chain
+  extend Witchcraft.Applicative
+  extend Witchcraft.Chain
 
-#   alias Witchcraft.Monad.AST
-#   import Witchcraft.Chain
+  alias Witchcraft.Monad.AST
+  import Witchcraft.Chain
 
-#   defmacro __using__(opts \\ []) do
-#     quote do
-#       use Witchcraft.Applicative, unquote(opts)
-#       use Witchcraft.Chain,       unquote(opts)
-#       import unquote(__MODULE__), unquote(opts)
-#     end
-#   end
+  defmacro __using__(opts \\ []) do
+    quote do
+      use Witchcraft.Applicative, unquote(opts)
+      use Witchcraft.Chain,       unquote(opts)
+      import unquote(__MODULE__), unquote(opts)
+    end
+  end
 
-#   @doc ~S"""
-#   do-notation enhanced with a `return` operation.
+  @doc ~S"""
+  do-notation enhanced with a `return` operation.
 
-#   `return` is the simplest possible linking function, providing the correct `of/2`
-#   instance for your monad.
+  `return` is the simplest possible linking function, providing the correct `of/2`
+  instance for your monad.
 
-#   ## Examples
+  ## Examples
 
-#       iex> monad [] do
-#       ...>   [1, 2, 3]
-#       ...> end
-#       [1, 2, 3]
+      iex> monad [] do
+      ...>   [1, 2, 3]
+      ...> end
+      [1, 2, 3]
 
-#       iex> monad [] do
-#       ...>   [1, 2, 3]
-#       ...>   [4, 5, 6]
-#       ...>   [7, 8, 9]
-#       ...> end
-#       [
-#         7, 8, 9,
-#         7, 8, 9,
-#         7, 8, 9,
-#         7, 8, 9,
-#         7, 8, 9,
-#         7, 8, 9,
-#         7, 8, 9,
-#         7, 8, 9,
-#         7, 8, 9
-#       ]
+      iex> monad [] do
+      ...>   [1, 2, 3]
+      ...>   [4, 5, 6]
+      ...>   [7, 8, 9]
+      ...> end
+      [
+        7, 8, 9,
+        7, 8, 9,
+        7, 8, 9,
+        7, 8, 9,
+        7, 8, 9,
+        7, 8, 9,
+        7, 8, 9,
+        7, 8, 9,
+        7, 8, 9
+      ]
 
-#       iex> monad [] do
-#       ...>   Witchcraft.Applicative.of([], 1)
-#       ...> end
-#       [1]
+      iex> monad [] do
+      ...>   Witchcraft.Applicative.of([], 1)
+      ...> end
+      [1]
 
-#       iex> monad [] do
-#       ...>  a <- [1,2,3]
-#       ...>  b <- [4,5,6]
-#       ...>  return(a * b)
-#       ...> end
-#       [
-#         4, 8,  12,
-#         5, 10, 15,
-#         6, 12, 18
-#       ]
+      iex> monad [] do
+      ...>  a <- [1,2,3]
+      ...>  b <- [4,5,6]
+      ...>  return(a * b)
+      ...> end
+      [
+        4, 8,  12,
+        5, 10, 15,
+        6, 12, 18
+      ]
 
-#       iex> monad [] do
-#       ...>   a <- return 1
-#       ...>   b <- return 2
-#       ...>   return(a + b)
-#       ...> end
-#       [3]
+      iex> monad [] do
+      ...>   a <- return 1
+      ...>   b <- return 2
+      ...>   return(a + b)
+      ...> end
+      [3]
 
-#   """
-#   defmacro monad(sample, do: input) do
-#     preprocessed = AST.preprocess(input, sample)
-#     quote do: Witchcraft.Chain.chain(do: unquote(preprocessed))
-#   end
+  """
+  defmacro monad(sample, do: input) do
+    preprocessed = AST.preprocess(input, sample)
+    quote do: Witchcraft.Chain.chain(do: unquote(preprocessed))
+  end
 
-#   properties do
-#     import Witchcraft.Applicative
-#     import Witchcraft.Chain
+  properties do
+    import Witchcraft.Applicative
+    import Witchcraft.Chain
 
-#     def left_identity(data) do
-#       a = generate(data)
-#       f = &Witchcraft.Functor.replace(a, inspect(&1))
+    def left_identity(data) do
+      a = generate(data)
+      f = &Witchcraft.Functor.replace(a, inspect(&1))
 
-#       left  = a |> of(a) |> chain(f)
-#       right = f.(a)
+      left  = a |> of(a) |> chain(f)
+      right = f.(a)
 
-#       equal?(left, right)
-#     end
+      equal?(left, right)
+    end
 
-#     def right_identity(data) do
-#       a = generate(data)
-#       left = a >>> &of(a, &1)
+    def right_identity(data) do
+      a = generate(data)
+      left = a >>> &of(a, &1)
 
-#       equal?(a, left)
-#     end
-#   end
-# end
+      equal?(a, left)
+    end
+  end
+end
 
-# definst Witchcraft.Monad, for: Function
-# definst Witchcraft.Monad, for: List
+definst Witchcraft.Monad, for: Function
+definst Witchcraft.Monad, for: List
 
-# definst Witchcraft.Monad, for: Tuple do
-#   use Witchcraft.Semigroup
-#   import TypeClass.Property.Generator, only: [generate: 1]
+definst Witchcraft.Monad, for: Tuple do
+  use Witchcraft.Semigroup
+  import TypeClass.Property.Generator, only: [generate: 1]
 
-#   custom_generator(_) do
-#     {generate(""), generate("")}
-#   end
-# end
+  custom_generator(_) do
+    {generate(""), generate("")}
+  end
+end
--- lib/witchcraft/traversable.ex
+++ lib/witchcraft/traversable.ex
@@ -1,307 +1,307 @@
-# import TypeClass
+import TypeClass
 
-# defclass Witchcraft.Traversable do
-#   @moduledoc """
-#   Walk through a data structure from left to right,
-#   running some action on each element in turn.
+defclass Witchcraft.Traversable do
+  @moduledoc """
+  Walk through a data structure from left to right,
+  running some action on each element in turn.
 
-#   Similar to applicatives, it can be used to do things like collecting some effect
-#   while performing other actions.
+  Similar to applicatives, it can be used to do things like collecting some effect
+  while performing other actions.
 
-#   ## Type Class
+  ## Type Class
 
-#   An instance of `Witchcraft.Traversable` must also implement `Witchcraft.Foldable`
-#   and `Witchcraft.Functor`, and define `Witchcraft.Foldable.right_fold/3`.
+  An instance of `Witchcraft.Traversable` must also implement `Witchcraft.Foldable`
+  and `Witchcraft.Functor`, and define `Witchcraft.Foldable.right_fold/3`.
 
-#       [right_fold/3]  Foldable    Functor  [map/2]
-#                              ↓    ↓
-#                            Traversable
-#                          [right_fold/3]
-#   """
+      [right_fold/3]  Foldable    Functor  [map/2]
+                             ↓    ↓
+                           Traversable
+                         [right_fold/3]
+  """
 
-#   alias __MODULE__
-#   alias Witchcraft.{Foldable, Unit}
+  alias __MODULE__
+  alias Witchcraft.{Foldable, Unit}
 
-#   extend Witchcraft.Foldable
-#   extend Witchcraft.Functor
+  extend Witchcraft.Foldable
+  extend Witchcraft.Functor
 
-#   use Witchcraft.Applicative
-#   use Witchcraft.Foldable, except: [equal?: 2]
+  use Witchcraft.Applicative
+  use Witchcraft.Foldable, except: [equal?: 2]
 
-#   use Quark
+  use Quark
 
-#   @type t :: any()
-#   @type link :: (any() -> Traversable.t())
+  @type t :: any()
+  @type link :: (any() -> Traversable.t())
 
-#   defmacro __using__(opts \\ []) do
-#     quote do
-#       use Witchcraft.Foldable,    unquote(opts)
-#       use Witchcraft.Functor,     unquote(opts)
-#       import unquote(__MODULE__), unquote(opts)
-#     end
-#   end
+  defmacro __using__(opts \\ []) do
+    quote do
+      use Witchcraft.Foldable,    unquote(opts)
+      use Witchcraft.Functor,     unquote(opts)
+      import unquote(__MODULE__), unquote(opts)
+    end
+  end
 
-#   where do
-#     @doc """
-#     Convert elements to actions, and then evaluate the actions from left-to-right,
-#     and accumulate the results.
+  where do
+    @doc """
+    Convert elements to actions, and then evaluate the actions from left-to-right,
+    and accumulate the results.
 
-#     For a version without accumulation, see `then_traverse/2`.
+    For a version without accumulation, see `then_traverse/2`.
 
-#     ## Examples
+    ## Examples
 
-#         iex> traverse([1, 2, 3], fn x -> {x, x * 2, x * 10} end)
-#         {6, 12, [10, 20, 30]}
+        iex> traverse([1, 2, 3], fn x -> {x, x * 2, x * 10} end)
+        {6, 12, [10, 20, 30]}
 
-#         iex> traverse({1, 2, 3}, fn x -> [x] end)
-#         [{1, 2, 3}]
+        iex> traverse({1, 2, 3}, fn x -> [x] end)
+        [{1, 2, 3}]
 
-#         iex> traverse({1, 2, 3}, fn x -> [x, x * 5, x * 10] end)
-#         [
-#           {1, 2, 3},
-#           {1, 2, 15},
-#           {1, 2, 30}
-#         ]
+        iex> traverse({1, 2, 3}, fn x -> [x, x * 5, x * 10] end)
+        [
+          {1, 2, 3},
+          {1, 2, 15},
+          {1, 2, 30}
+        ]
 
-#         iex> traverse([1, 2, 3], fn x -> [x, x * 5, x * 10] end)
-#         [
-#           #
-#           [1, 2,  3], [1, 2,  15], [1, 2,  30],
-#           [1, 10, 3], [1, 10, 15], [1, 10, 30],
-#           [1, 20, 3], [1, 20, 15], [1, 20, 30],
-#           #
-#           [5, 2,  3], [5, 2,  15], [5, 2,  30],
-#           [5, 10, 3], [5, 10, 15], [5, 10, 30],
-#           [5, 20, 3], [5, 20, 15], [5, 20, 30],
-#           #
-#           [10, 2,  3], [10, 2,  15], [10, 2,  30],
-#           [10, 10, 3], [10, 10, 15], [10, 10, 30],
-#           [10, 20, 3], [10, 20, 15], [10, 20, 30]
-#         ]
+        iex> traverse([1, 2, 3], fn x -> [x, x * 5, x * 10] end)
+        [
+          #
+          [1, 2,  3], [1, 2,  15], [1, 2,  30],
+          [1, 10, 3], [1, 10, 15], [1, 10, 30],
+          [1, 20, 3], [1, 20, 15], [1, 20, 30],
+          #
+          [5, 2,  3], [5, 2,  15], [5, 2,  30],
+          [5, 10, 3], [5, 10, 15], [5, 10, 30],
+          [5, 20, 3], [5, 20, 15], [5, 20, 30],
+          #
+          [10, 2,  3], [10, 2,  15], [10, 2,  30],
+          [10, 10, 3], [10, 10, 15], [10, 10, 30],
+          [10, 20, 3], [10, 20, 15], [10, 20, 30]
+        ]
 
-#         traverse([1, 2, 3], fn x -> %Algae.Maybe.Just{just: x} end)
-#         #=> %Algae.Maybe.Just{just: [1, 2, 3]}
+        traverse([1, 2, 3], fn x -> %Algae.Maybe.Just{just: x} end)
+        #=> %Algae.Maybe.Just{just: [1, 2, 3]}
 
-#         traverse(%Algae.Maybe.Just{just: 4}, fn x -> [x, x * 10] end)
-#         #=> [
-#         #     %Algae.Maybe.Just{just: 4},
-#         #     %Algae.Maybe.Just{just: 40}
-#         #   ]
+        traverse(%Algae.Maybe.Just{just: 4}, fn x -> [x, x * 10] end)
+        #=> [
+        #     %Algae.Maybe.Just{just: 4},
+        #     %Algae.Maybe.Just{just: 40}
+        #   ]
 
-#         traverse([1, 2, 3], fn x ->
-#           if is_even(x) do
-#             %Algae.Maybe.Just{just: x}
-#           else
-#             %Algae.Maybe.Nothing{}
-#           end
-#         end)
-#         #=> %Algae.Maybe.Nothing{}
+        traverse([1, 2, 3], fn x ->
+          if is_even(x) do
+            %Algae.Maybe.Just{just: x}
+          else
+            %Algae.Maybe.Nothing{}
+          end
+        end)
+        #=> %Algae.Maybe.Nothing{}
 
-#     """
-#     @spec traverse(Traversable.t(), Traversable.link()) :: Traversable.t()
-#     def traverse(data, link)
-#   end
+    """
+    @spec traverse(Traversable.t(), Traversable.link()) :: Traversable.t()
+    def traverse(data, link)
+  end
 
-#   properties do
-#     def naturality(data) do
-#       a = generate(data)
+  properties do
+    def naturality(data) do
+      a = generate(data)
 
-#       f = fn x -> [x] end
-#       g = &Quark.id/1
+      f = fn x -> [x] end
+      g = &Quark.id/1
 
-#       a
-#       |> Traversable.traverse(f)
-#       |> g.()
-#       |> equal?(Traversable.traverse(a, fn x -> x |> f.() |> g.() end))
-#     end
+      a
+      |> Traversable.traverse(f)
+      |> g.()
+      |> equal?(Traversable.traverse(a, fn x -> x |> f.() |> g.() end))
+    end
 
-#     def identity(data) do
-#       a = generate(data)
+    def identity(data) do
+      a = generate(data)
 
-#       a
-#       |> Traversable.traverse(fn x -> {x} end)
-#       |> equal?({a})
-#     end
+      a
+      |> Traversable.traverse(fn x -> {x} end)
+      |> equal?({a})
+    end
 
-#     def composition(_data) do
-#       # traverse (Compose . fmap g . f) = Compose . fmap (traverse g) . traverse f
+    def composition(_data) do
+      # traverse (Compose . fmap g . f) = Compose . fmap (traverse g) . traverse f
 
-#       # MAINTAINER COMMENT
-#       # ==================
-#       #
-#       # I cannot get this working. After throwing 1.5 days at this *one* prop,
-#       # I'm walking away for now. If someone has an implementation that works,
-#       # it would be greatly appreciated.
-#       #
-#       # I've implemented %Endo{} and %Compose{}, but Compose's Foldable refused
-#       # to work because there is no Foldable instance for functions (AFAIK).
-#       #
-#       # I believe that the Hakell version works because of the lazy
-#       # recursive call to foldMap in `Foldable f, Foldable g => Compose (f g)`,
-#       # which in Elixir is trying to get dispatched on the partial function.
-#       #
-#       # It's very possible that there's a simple solution that I'm just not seeing
-#       # after staring at the problem for too long
-#       #
-#       # PRs gladly accepted here: https://github.com/expede/witchcraft/compare
-#       #
-#       # ~ Brooklyn Zelenka, @expede
+      # MAINTAINER COMMENT
+      # ==================
+      #
+      # I cannot get this working. After throwing 1.5 days at this *one* prop,
+      # I'm walking away for now. If someone has an implementation that works,
+      # it would be greatly appreciated.
+      #
+      # I've implemented %Endo{} and %Compose{}, but Compose's Foldable refused
+      # to work because there is no Foldable instance for functions (AFAIK).
+      #
+      # I believe that the Hakell version works because of the lazy
+      # recursive call to foldMap in `Foldable f, Foldable g => Compose (f g)`,
+      # which in Elixir is trying to get dispatched on the partial function.
+      #
+      # It's very possible that there's a simple solution that I'm just not seeing
+      # after staring at the problem for too long
+      #
+      # PRs gladly accepted here: https://github.com/expede/witchcraft/compare
+      #
+      # ~ Brooklyn Zelenka, @expede
 
-#       true
-#     end
-#   end
+      true
+    end
+  end
 
-#   # traverse with args flipped
-#   @doc """
-#   `traverse/2` with arguments reversed.
+  # traverse with args flipped
+  @doc """
+  `traverse/2` with arguments reversed.
 
-#   ## Examples
+  ## Examples
 
-#       iex> fn x -> {x, x * 2, x * 10} end |> through([1, 2, 3])
-#       {6, 12, [10, 20, 30]}
+      iex> fn x -> {x, x * 2, x * 10} end |> through([1, 2, 3])
+      {6, 12, [10, 20, 30]}
 
-#       iex> fn x -> [x] end |> through({1, 2, 3})
-#       [{1, 2, 3}]
+      iex> fn x -> [x] end |> through({1, 2, 3})
+      [{1, 2, 3}]
 
-#       iex> fn x -> [x, x * 5, x * 10] end |> through({1, 2, 3})
-#       [
-#         {1, 2, 3},
-#         {1, 2, 15},
-#         {1, 2, 30}
-#       ]
+      iex> fn x -> [x, x * 5, x * 10] end |> through({1, 2, 3})
+      [
+        {1, 2, 3},
+        {1, 2, 15},
+        {1, 2, 30}
+      ]
 
-#       iex> fn x -> [x, x * 5, x * 10] end |> through([1, 2, 3])
-#       [
-#         #
-#         [1, 2,  3], [1, 2,  15], [1, 2,  30],
-#         [1, 10, 3], [1, 10, 15], [1, 10, 30],
-#         [1, 20, 3], [1, 20, 15], [1, 20, 30],
-#         #
-#         [5, 2,  3], [5, 2,  15], [5, 2,  30],
-#         [5, 10, 3], [5, 10, 15], [5, 10, 30],
-#         [5, 20, 3], [5, 20, 15], [5, 20, 30],
-#         #
-#         [10, 2,  3], [10, 2,  15], [10, 2,  30],
-#         [10, 10, 3], [10, 10, 15], [10, 10, 30],
-#         [10, 20, 3], [10, 20, 15], [10, 20, 30]
-#       ]
+      iex> fn x -> [x, x * 5, x * 10] end |> through([1, 2, 3])
+      [
+        #
+        [1, 2,  3], [1, 2,  15], [1, 2,  30],
+        [1, 10, 3], [1, 10, 15], [1, 10, 30],
+        [1, 20, 3], [1, 20, 15], [1, 20, 30],
+        #
+        [5, 2,  3], [5, 2,  15], [5, 2,  30],
+        [5, 10, 3], [5, 10, 15], [5, 10, 30],
+        [5, 20, 3], [5, 20, 15], [5, 20, 30],
+        #
+        [10, 2,  3], [10, 2,  15], [10, 2,  30],
+        [10, 10, 3], [10, 10, 15], [10, 10, 30],
+        [10, 20, 3], [10, 20, 15], [10, 20, 30]
+      ]
 
-#   """
-#   @spec through(Traversable.link(), Traversable.t()) :: Traversable.t()
-#   def through(link, traversable), do: traverse(traversable, link)
+  """
+  @spec through(Traversable.link(), Traversable.t()) :: Traversable.t()
+  def through(link, traversable), do: traverse(traversable, link)
 
-#   @doc """
-#   `traverse` actions over data, but ignore the results.
+  @doc """
+  `traverse` actions over data, but ignore the results.
 
-#   ## Examples
+  ## Examples
 
-#       iex> [1, 2, 3]
-#       ...> |> then_traverse(fn x -> [x, x * 5, x * 10] end)
-#       [
-#           #
-#           %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
-#           %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
-#           %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
-#           #
-#           %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
-#           %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
-#           %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
-#           #
-#           %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
-#           %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
-#           %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{}
-#       ]
+      iex> [1, 2, 3]
+      ...> |> then_traverse(fn x -> [x, x * 5, x * 10] end)
+      [
+          #
+          %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
+          %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
+          %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
+          #
+          %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
+          %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
+          %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
+          #
+          %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
+          %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
+          %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{}
+      ]
 
-#   """
-#   @spec then_traverse(Traversable.t(), Traversable.link()) :: Applicative.t()
-#   def then_traverse(traversable, link) do
-#     right_fold(traversable, of(traversable, %Unit{}), fn(step, acc) ->
-#       step
-#       |> link.()
-#       |> then(acc)
-#     end)
-#   end
+  """
+  @spec then_traverse(Traversable.t(), Traversable.link()) :: Applicative.t()
+  def then_traverse(traversable, link) do
+    right_fold(traversable, of(traversable, %Unit{}), fn(step, acc) ->
+      step
+      |> link.()
+      |> then(acc)
+    end)
+  end
 
-#   @doc """
-#   The same as `then_traverse`, but with the arguments flipped.
+  @doc """
+  The same as `then_traverse`, but with the arguments flipped.
 
-#   ## Examples
+  ## Examples
 
-#       iex> fn x -> [x, x * 5, x * 10] end
-#       ...> |> then_through([1, 2, 3])
-#       [
-#           #
-#           %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
-#           %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
-#           %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
-#           #
-#           %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
-#           %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
-#           %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
-#           #
-#           %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
-#           %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
-#           %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{}
-#       ]
+      iex> fn x -> [x, x * 5, x * 10] end
+      ...> |> then_through([1, 2, 3])
+      [
+          #
+          %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
+          %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
+          %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
+          #
+          %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
+          %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
+          %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
+          #
+          %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
+          %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{},
+          %Witchcraft.Unit{}, %Witchcraft.Unit{}, %Witchcraft.Unit{}
+      ]
 
-#   """
-#   @spec then_traverse(Traversable.link(), Traversable.t()) :: Applicative.t()
-#   def then_through(link, traversable), do: then_traverse(traversable, link)
+  """
+  @spec then_traverse(Traversable.link(), Traversable.t()) :: Applicative.t()
+  def then_through(link, traversable), do: then_traverse(traversable, link)
 
-#   @doc """
-#   Run each action/effect in sequence (from left to right),
-#   and accumulate values along the way.
+  @doc """
+  Run each action/effect in sequence (from left to right),
+  and accumulate values along the way.
 
-#   ## Examples
+  ## Examples
 
-#       iex> sequence([{1, 2, 3}, {4, 5, 6}])
-#       {5, 7, [3, 6]}
+      iex> sequence([{1, 2, 3}, {4, 5, 6}])
+      {5, 7, [3, 6]}
 
-#       iex> [
-#       ...>   [1, 2, 3],
-#       ...>   [4, 5, 6]
-#       ...> ]
-#       ...> |> sequence()
-#       [
-#         [1, 4],
-#         [1, 5],
-#         [1, 6],
-#         [2, 4],
-#         [2, 5],
-#         [2, 6],
-#         [3, 4],
-#         [3, 5],
-#         [3, 6]
-#       ]
+      iex> [
+      ...>   [1, 2, 3],
+      ...>   [4, 5, 6]
+      ...> ]
+      ...> |> sequence()
+      [
+        [1, 4],
+        [1, 5],
+        [1, 6],
+        [2, 4],
+        [2, 5],
+        [2, 6],
+        [3, 4],
+        [3, 5],
+        [3, 6]
+      ]
 
-#   """
-#   @spec sequence(Traversable.t()) :: Foldable.t()
-#   def sequence(traversable), do: traverse(traversable, &Quark.id/1)
-# end
+  """
+  @spec sequence(Traversable.t()) :: Foldable.t()
+  def sequence(traversable), do: traverse(traversable, &Quark.id/1)
+end
 
-# definst Witchcraft.Traversable, for: Tuple do
-#   use Witchcraft.Functor
+definst Witchcraft.Traversable, for: Tuple do
+  use Witchcraft.Functor
 
-#   def traverse(tuple, link) do
-#     last_index = tuple_size(tuple) - 1
+  def traverse(tuple, link) do
+    last_index = tuple_size(tuple) - 1
 
-#     tuple
-#     |> elem(last_index)
-#     |> link.()
-#     ~> fn last -> put_elem(tuple, last_index, last) end
-#   end
-# end
+    tuple
+    |> elem(last_index)
+    |> link.()
+    ~> fn last -> put_elem(tuple, last_index, last) end
+  end
+end
 
-# definst Witchcraft.Traversable, for: List do
-#   use Witchcraft.Applicative
-#   use Witchcraft.Foldable
+definst Witchcraft.Traversable, for: List do
+  use Witchcraft.Applicative
+  use Witchcraft.Foldable
 
-#   def traverse([], link), do: of(link.([]), [])
-#   def traverse(list = [head | _], link) do
-#     right_fold(list, of(link.(head), []), fn(x, acc) ->
-#       lift(link.(x), acc, fn(link_head, link_tail) ->
-#         [link_head | link_tail]
-#       end)
-#     end)
-#   end
-# end
+  def traverse([], link), do: of(link.([]), [])
+  def traverse(list = [head | _], link) do
+    right_fold(list, of(link.(head), []), fn(x, acc) ->
+      lift(link.(x), acc, fn(link_head, link_tail) ->
+        [link_head | link_tail]
+      end)
+    end)
+  end
+end
