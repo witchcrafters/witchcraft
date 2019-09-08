77a205541f13677a008f92d52b2a0cec4a2eb1f9
Steven Vandevelde <icid.asset@gmail.com>
2019-09-08 19:31:00 +0200

    1.0.2

--- mix.exs
+++ mix.exs
@@ -4,35 +4,58 @@ defmodule Witchcraft.Mixfile do
   def project do
     [
       app: :witchcraft,
+      aliases: aliases(),
+      deps: deps(),
+
+      # Versions
+      version: "1.0.2",
+      elixir: "~> 1.9",
+
+      # Docs
       name: "Witchcraft",
+      docs: docs(),
+
+      # Hex
       description: "Monads and other dark magic (monoids, functors, traversables, &c)",
-      version: "1.0.1",
-      elixir: "~> 1.5",
-      package: [
-        maintainers: ["Brooklyn Zelenka"],
-        licenses: ["MIT"],
-        links: %{"GitHub" => "https://github.com/expede/witchcraft"}
-      ],
-      source_url: "https://github.com/expede/witchcraft",
-      homepage_url: "https://github.com/expede/witchcraft",
-      aliases: [quality: ["test", "credo --strict", "inch"]],
-      deps: [
-        {:inch_ex, "~> 0.5", only: [:dev, :docs, :test]},
-        {:credo, "~> 0.8", only: [:dev, :test]},
-        {:benchfella, "~> 0.3", only: [:dev, :test]},
-        {:dialyxir, "~> 0.3", only: :dev},
-        {:earmark, "~> 1.2", only: :dev},
-        {:ex_doc, "~> 0.15", only: :dev},
-        {:exceptional, "~> 2.1"},
-        {:operator, "~> 0.2"},
-        {:quark, "~> 2.2"},
-        {:type_class, "~> 1.2"}
-      ],
-      docs: [
-        extras: ["README.md"],
-        logo: "./brand/Icon/PNG/WC-icon-sml@2x-circle.png",
-        main: "readme"
-      ]
+      package: package()
+    ]
+  end
+
+  defp aliases do
+    [
+      quality: ["test", "credo --strict", "inch"]
+    ]
+  end
+
+  defp deps do
+    [
+      {:inch_ex, "~> 2.0", only: [:dev, :docs, :test]},
+      {:credo, "~> 1.1.4", only: [:dev, :test]},
+      {:benchfella, "~> 0.3", only: [:dev, :test]},
+      {:dialyxir, "~> 0.3", only: :dev},
+      {:earmark, "~> 1.4.0", only: :dev},
+      {:ex_doc, "~> 0.21.2", only: :dev},
+      {:exceptional, "~> 2.1"},
+      {:operator, "~> 0.2"},
+      {:quark, "~> 2.2"},
+      {:type_class, "~> 1.2"}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md"],
+      logo: "./brand/Icon/PNG/WC-icon-sml@2x-circle.png",
+      main: "readme",
+      source_url: "https://github.com/witchcrafters/witchcraft"
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Brooklyn Zelenka"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/witchcrafters/witchcraft"}
     ]
   end
 end
