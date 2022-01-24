2a907ef6d2cb174a0e29ede6d465d27fb4237a86
Quinn Wilton <quinn@quinnwilton.com>
2022-01-23 17:28:37 -0800

    Disable `PipeIntoAnonymousFunctions` and `Apply` Credo rules
    The first is a pattern that's very common within the codebase,
    and the second is incompatible with Semigroupoid.apply/2

--- .credo.exs
+++ .credo.exs
@@ -9,7 +9,10 @@
       checks: [
         {Credo.Check.Consistency.TabsOrSpaces},
         {Credo.Check.Design.AliasUsage, false},
-        {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 100}
+        {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 100},
+        {Credo.Check.Readability.PipeIntoAnonymousFunctions, false},
+        # This rule incorrectly flags uses of Witchcraft.Semigroupoid.apply/2
+        {Credo.Check.Refactor.Apply, false}
       ]
     }
   ]
