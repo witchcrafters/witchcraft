4ddd4c87fe3f1415f41c4c963fc3ac2772405290
Jechol Lee <mr.jechol@gmail.com>
2022-01-28 17:07:03 +0900

    Add cachix instruction (#107)

--- shell.nix
+++ shell.nix
@@ -1,5 +1,6 @@
 let
   nixpkgs = import (fetchTarball {
+    # Run `cachix use jechol` to use compiled binary cache.
     url = "https://github.com/jechol/nixpkgs/archive/21.11-otp24-no-jit.tar.gz";
     sha256 = "sha256:1lka707hrnkp70vny99m9fmp4a8136vl7addmpfsdvkwb81d1jk9";
   }) { };
